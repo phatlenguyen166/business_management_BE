08166d10635035df6a3f408a92d40b330cf1f8ef
phatlenguyen166 <phatlenguyen166@gmail.com>
2025-02-17 13:54:00 +0700

    first commit

---
+++ src/test/java/vn/bookstore/app/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.bookstore.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
