e26105dc1f2e21ed4457cf9ebe06d2f7d2db4e54
phatlenguyen166 <phatlenguyen166@gmail.com>
2025-02-24 23:21:31 +0700

    change package model -> dto

--- src/main/java/vn/bookstore/app/util/error/GlobalException.java
+++ src/main/java/vn/bookstore/app/util/error/GlobalException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import vn.bookstore.app.model.response.RestResponse;
+import vn.bookstore.app.dto.response.RestResponse;
 
 @RestControllerAdvice
 public class GlobalException {
