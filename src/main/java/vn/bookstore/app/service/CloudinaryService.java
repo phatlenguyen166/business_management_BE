ebfc0b70a394c4fbe75985f3c1aa56697384abe6
phatlenguyen166 <phatlenguyen166@gmail.com>
2025-03-07 09:12:28 +0700

    minor fixed

--- src/main/java/vn/bookstore/app/service/CloudinaryService.java
+++ src/main/java/vn/bookstore/app/service/CloudinaryService.java
@@ -9,39 +9,10 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.util.Map;
 
-@Service
-public class CloudinaryService {
+public interface CloudinaryService {
+    String uploadFile(MultipartFile file) throws IOException;
     
-    private final Cloudinary cloudinary;
+    void deleteFile(String imageUrl) throws IOException;
     
-    public CloudinaryService(
-            @Value("${cloudinary.cloud-name}") String cloudName,
-            @Value("${cloudinary.api-key}") String apiKey,
-            @Value("${cloudinary.api-secret}") String apiSecret) {
-        this.cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret
-        ));
-    }
-    
-    public String uploadFile(MultipartFile file) throws IOException {
-        Map uploadResult = cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap());
-        return uploadResult.get("url").toString();
-    }
-    
-    public void deleteFile(String imageUrl) throws IOException {
-        try {
-            String publicId = extractPublicId(imageUrl);
-            cloudinary.uploader().destroy(publicId, ObjectUtils.emptyMap());
-        } catch (Exception e) {
-            throw new IOException("Lỗi khi xóa ảnh trên Cloudinary: " + e.getMessage());
-        }
-    }
-    
-    public String extractPublicId(String imageUrl) {
-        String[] parts = imageUrl.split("/");
-        String fileName = parts[parts.length - 1];
-        return fileName.split("\\.")[0];
-    }
+    String extractPublicId(String imageUrl);
 }
--- src/main/java/vn/bookstore/app/service/TokenService.java
+++ src/main/java/vn/bookstore/app/service/TokenService.java
@@ -7,30 +7,10 @@ import vn.bookstore.app.repository.TokenRepository;
 
 import java.util.Optional;
 
-@Service
-public record TokenService(TokenRepository tokenRepository) {
-    public long save(Token token) {
-        Optional<Token> optional = tokenRepository.findByUsername(token.getUsername());
-        
-        if (optional.isEmpty()) {
-            tokenRepository.save(token);
-            return token.getId();
-        } else {
-            Token currentToken = optional.get();
-            currentToken.setAccessToken(token.getAccessToken());
-            currentToken.setRefreshToken(token.getRefreshToken());
-            tokenRepository.save(currentToken);
-            return currentToken.getId();
-        }
-    }
+public interface TokenService {
+    public long save(Token token);
     
-    public void delete(String username) {
-        Token token = getByUsername(username);
-        tokenRepository.delete(token);
-    }
+    public void delete(String username);
     
-    public Token getByUsername(String username) {
-        return tokenRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not" +
-                " found"));
-    }
+    public Token getByUsername(String username);
 }
