2c8ddc1d63becee725d5aa599ea24a04af70423b
Hoang Phat <phatlenguyen166@gmail.com>
2025-03-29 01:02:08 +0700

    Fix API Login

--- src/main/java/vn/bookstore/app/mapper/TokenMapper.java
+++ src/main/java/vn/bookstore/app/mapper/TokenMapper.java
@@ -5,17 +5,16 @@ import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import vn.bookstore.app.dto.response.ResTokenDTO;
 import vn.bookstore.app.model.Role;
-import vn.bookstore.app.model.Token;
 import vn.bookstore.app.model.User;
-
 
 @Mapper(componentModel = "spring")
 public interface TokenMapper {
+
     TokenMapper INSTANCE = Mappers.getMapper(TokenMapper.class);
-    
+
     @Mapping(source = "user.id", target = "userId")
-    @Mapping(source = "role", target = "roleInfo")
     @Mapping(source = "accessToken", target = "accessToken")
     @Mapping(source = "refreshToken", target = "refreshToken")
+    @Mapping(source = "role.name", target = "roleName")
     ResTokenDTO mapToResTokenDTO(User user, String accessToken, String refreshToken, Role role);
 }
