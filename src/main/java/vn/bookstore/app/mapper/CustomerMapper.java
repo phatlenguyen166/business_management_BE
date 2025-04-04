09985078eb581a2c9f7281bf67f24bed1fde73ae
Hoang Phat <phatlenguyen166@gmail.com>
2025-04-05 03:32:58 +0700

    Fix Api Customer

--- src/main/java/vn/bookstore/app/mapper/CustomerMapper.java
+++ src/main/java/vn/bookstore/app/mapper/CustomerMapper.java
@@ -1,20 +1,25 @@
 package vn.bookstore.app.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+
 import vn.bookstore.app.dto.request.ReqCustomerDTO;
-import vn.bookstore.app.dto.request.ReqSupplierDTO;
 import vn.bookstore.app.dto.response.ResCustomerDTO;
 import vn.bookstore.app.model.Customer;
-import vn.bookstore.app.model.Supplier;
 
 @Mapper(componentModel = "spring")
 public interface CustomerMapper {
-    
+
     Customer toCustomer(ReqCustomerDTO reqCustomerDTO);
-    
+
+    @Mapping(source = "id", target = "idString", qualifiedByName = "longToString")
     ResCustomerDTO toResCustomerDTO(Customer customer);
-    
+
     void updateCustomerFromDto(ReqCustomerDTO reqCustomerDTO, @MappingTarget Customer customer);
-    
+
+    @org.mapstruct.Named("longToString")
+    default String longToString(Long value) {
+        return value != null ? "KH-" + value : null;
+    }
 }
